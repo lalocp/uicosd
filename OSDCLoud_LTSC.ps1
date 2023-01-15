86d85344a321c9dde539f0f313d87de3d170f737
lalocp <lalo.camacho@gmail.com>
2023-01-14 18:48:19 -0600

    Update OSDCLoud_LTSC.ps1

--- OSDCLoud_LTSC.ps1
+++ OSDCLoud_LTSC.ps1
@@ -25,4 +25,4 @@ Start-OSDCloud -OSLanguage en-us -OSBuild 22H2 -OSEdition Enterprise LTSC -OSLic
 #Restart from WinPE
 Write-Host   -ForegroundColor Cyan "Restarting in 20 seconds!"
 Start-Sleep  -Seconds 20
-wpeutil reboot
+#wpeutil reboot
