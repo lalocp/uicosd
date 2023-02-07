b02b2136eebac4552431f72d77fcae2edaa9ad9b
lalocp <lalo.camacho@gmail.com>
2023-02-06 19:29:49 -0600

    Update OSDCLoud_LTSC.ps1

--- OSDCLoud_LTSC.ps1
+++ OSDCLoud_LTSC.ps1
@@ -1,9 +1,15 @@
 Write-Host  -ForegroundColor Cyan "UIC Tech. Solutions: Windows Deployment process using OSDCloud."
 Start-Sleep -Seconds 5
 #Change Display Resolution for Virtual Machine
-if ((Get-MyComputerModel) -match 'Virtual') {
-    Write-Host  -ForegroundColor Cyan "Setting Display Resolution to 1600x"
-    Set-DisRes 1600
+
+try {
+    if ((Get-MyComputerModel) -match 'Virtual') {
+        Write-Host  -ForegroundColor Cyan "Setting Display Resolution to 1600x"
+        Set-DisRes 1600
+    }
+}
+catch {
+      $error | out-null
 }
 
 #Make sure I have the latest OSD Content
@@ -11,7 +17,12 @@ Write-Host  -ForegroundColor Cyan "Updating the awesome OSD PowerShell Module"
 Install-Module OSD -Force
 
 Write-Host  -ForegroundColor Cyan "Importing the sweet OSD PowerShell Module"
-Import-Module OSD -Force
+try {
+    Import-Module OSD -Force
+    }
+catch {
+        $error | out-null
+    }
  
 #Start OSDCloud ZTI the RIGHT way
 Write-Host  -ForegroundColor Cyan "Starting OSDCloud GUI"
