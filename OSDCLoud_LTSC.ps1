77c523978a57c1016de029a5481d4619d346e00a
lalocp <lalo.camacho@gmail.com>
2023-02-06 19:15:02 -0600

    Update OSDCLoud_LTSC.ps1

--- OSDCLoud_LTSC.ps1
+++ OSDCLoud_LTSC.ps1
@@ -1,4 +1,5 @@
 Write-Host  -ForegroundColor Cyan "UIC Tech. Solutions: Windows Deployment process using OSDCloud."
+Start-Sleep -Seconds 5
 #Change Display Resolution for Virtual Machine
 if ((Get-MyComputerModel) -match 'Virtual') {
     Write-Host  -ForegroundColor Cyan "Setting Display Resolution to 1600x"
@@ -16,11 +17,11 @@ Import-Module OSD -Force
 Write-Host  -ForegroundColor Cyan "Starting OSDCloud GUI"
 Write-Host  -ForegroundColor Cyan "Choose your preferred version of Windows Enterprise"
 Write-Host  -ForegroundColor Cyan "Very Important, to decrease the potential for a Blue Screen of Death crash"
-Write-Host  -ForegroundColor Cyan "Choose 'Microsoft Update Catalog' for the 'DriverPack'"
+Write-Host  -ForegroundColor Cyan "Choose Microsoft Update Catalog for the DriverPack"
 
 Start-Sleep -Seconds 120
 Start-OSDCloudGUI
 
-write-host   -ForegroundColor cyan "Before pulling out the USB drive, shutdown the computer with this command:
-write-host   -ForegroundColor cyan "'wpeutil shutdown' in a command prompt"
+write-host   -ForegroundColor cyan "Before pulling out the USB drive, shutdown the computer with this command"
+write-host   -ForegroundColor cyan "wpeutil shutdown in a command prompt"
 Start-Sleep  -Seconds 60
