1f45c29094f94ff55f3c18e7cc71a8d9da091544
lalocp <lalo.camacho@gmail.com>
2023-03-22 12:32:45 -0500

    Update New_OS_Install.ps1

--- New_OS_Install.ps1
+++ New_OS_Install.ps1
@@ -35,13 +35,14 @@ Write-Host  -ForegroundColor Cyan "Installing Windows 10 22H2 Enterprise"
 Start-Sleep -Seconds 5
 
 Start-OSDCloud -OSLanguage en-us -OSBuild 22H2 -OSEdition Enterprise -OSLicense Volume -ZTI -OSVersion 'Windows 10' -Manufacturer None -Product None
+Write-Host  ""
+# Add support for message boxes
+    Add-Type -AssemblyName PresentationCore,PresentationFramework
+# Display message about shutdown and removing flash drive
 
-write-host ""
-write-host -ForegroundColor Yellow "ATTENTION - PLEASE READ"
-write-host -ForegroundColor cyan "Before booting the new Windows Install, remove the USB drive after first shutting down the computer."
-write-host ""
-write-host -ForegroundColor cyan "Before disconnecting the USB drive, shutdown the computer using the following command in the black"
-write-host -Foregroundcolor cyan -NoNewline "'command' window after this blue window closes:"
-write-host -ForegroundColor RED  -NoNewlin " wpeutil shutdown"
-write-host ""
-Start-Sleep  -Seconds 10
+[void][System.Windows.MessageBox]::Show('                             !!! ATTENTION - PLEASE READ !!!
+Before disconnecting the USB drive, shutdown the computer by typing
+                                      WPEUTIL SHUTDOWN
+in the command prompt window after clicking OK.')
+Start-Sleep -Seconds 1
+start-process -FilePath "$env:comspec" -Argumentlist "/k color e4 & echo type: 'wpeutil shutdown' without quotes, to shutdown this computer."
