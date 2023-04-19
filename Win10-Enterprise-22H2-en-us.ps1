860ad5404295e3a5d006ebfb62614f21535ed9c7
lalocp <lalo.camacho@gmail.com>
2023-04-19 16:29:22 -0500

    Update Win10-Enterprise-22H2-en-us.ps1

--- Win10-Enterprise-22H2-en-us.ps1
+++ Win10-Enterprise-22H2-en-us.ps1
@@ -1,11 +1,24 @@
-Write-Host  -ForegroundColor Cyan "UIC Tech. Solutions OS Deployment using OSDCloud."
-Write-Host  -ForegroundColor Cyan "Installing OS: Windows 10 Enterprise 22H2"
-Start-Sleep -Seconds 5
-
-#Change Display Resolution for Virtual Machine
-if ((Get-MyComputerModel) -match 'Virtual') {
-    Write-Host  -ForegroundColor Cyan "Setting Display Resolution to 1600x"
-    Set-DisRes 1600
+Write-Host ""
+Write-Host ""
+Write-Host ""
+Write-Host -ForegroundColor Yellow -BackgroundColor REd "EAS Tech. Solutions: Windows Deployment process using OSDCloud."
+Write-Host ""
+Write-Host ""
+# Change Display Resolution for Virtual Machine
+try {
+    if ((Get-MyComputerModel) -like '*Virtual*') {
+        try {
+        if (Set-DisRes 1600 -Verbose:$false | out-null){
+                Write-Host  -ForegroundColor Cyan "Setting Display Resolution to 1600x"
+                }
+        }
+        catch {
+             $error | out-null
+             }
+    }
+}
+catch {
+      $error | out-null
 }
 
 #Make sure I have the latest OSD Content
@@ -14,26 +27,25 @@ Write-Host  -ForegroundColor Cyan "Updating the awesome OSD PowerShell Module"
 
 try {
     if (Install-Module OSD -Force -ErrorAction SilentlyContinue -Verbose:$false | out-null) {
-          Import-Module OSD -Force -ErrorAction SilentlyContinue -Verbose:$false |out-null
+          Write-Host -ForegroundColor Cyan "Updating the awesome OSD PowerShell Module"
+          if (Import-Module OSD -Force -ErrorAction SilentlyContinue -Verbose:$false | out-null) {
+            Write-Host -ForegroundColor Cyan "Importing the sweet OSD PowerShell Module"
+            }
         }
     }
 catch {
         $error | out-null
-    }
-
-Write-Host  -ForegroundColor Cyan "Importing the sweet OSD PowerShell Module"
-Import-Module OSD -Force
- 
+    } 
 #Start OSDCloud ZTI the RIGHT way
-Write-Host  -ForegroundColor Cyan "Start OSDCloud with custom parameters"
-Write-Host  -ForegroundColor Cyan "OS: Windows 10 Enterprise 22H2"
 
-Start-OSDCloud -OSLanguage en-us -OSBuild 22H2 -OSEdition Enterprise -OSLicense Volume -ZTI -OSVersion 'Windows 10'
+Write-Host -ForegroundColor Yellow -BackgroundColor REd "Starting OSDCloud... This will completely erase the local hard drive. Press control C to cancel..."
+Write-Host -ForegroundColor Yellow -BackgroundColor REd ""
+Start-Sleep -Seconds 5
+Write-Host -ForegroundColor Yellow -BackgroundColor REd "Installing Windows 10 22H2 Enterprise"
+Start-Sleep -Seconds 2
 
-#Restart from WinPE
-#Write-Host -ForegroundColor Cyan "Restarting in 20 seconds!"
-#Write-host  -ForegroundColor Cyan "Use this link to download the Lenovo M920q driver pack"
-#write-host  -ForegroundColor Cyan "https://download.lenovo.com/pccbbs/thinkcentre_drivers/tc_m720tsq-m920tsxq-p330tiny_w1064_2004_202007.exe"
+Start-OSDCloud GUI -Brand "UIC Technology Solutions Computer Labs"
+
 Write-Host  -ForegroundColor Cyan " "
 Write-Host  -ForegroundColor Cyan " "
 Write-Host  -ForegroundColor Cyan "use 'wpeutil shutdown' and remove USB drive after power is off!"
@@ -41,5 +53,4 @@ Write-Host  -ForegroundColor Cyan " "
 Write-Host  -ForegroundColor Cyan " "
 Write-Host  -ForegroundColor Cyan " "
 Write-Host  -ForegroundColor Cyan " "
-Start-Sleep -Seconds 60
-#wpeutil reboot
+Start-Sleep -Seconds 30
