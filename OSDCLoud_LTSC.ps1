a19e822d71e6626151e0a2ac115f9ba87bcbfce5
lalocp <lalo.camacho@gmail.com>
2024-01-04 17:36:44 -0600

    Update OSDCLoud_LTSC.ps1
    added warning about autopilot json file

--- OSDCLoud_LTSC.ps1
+++ OSDCLoud_LTSC.ps1
@@ -1,5 +1,5 @@
 Write-Host  -ForegroundColor Cyan "UIC Tech. Solutions: Windows Deployment process using OSDCloud."
-Start-Sleep -Seconds 5
+Start-Sleep -Seconds 2
 #Change Display Resolution for Virtual Machine
 
 try {
@@ -25,13 +25,14 @@ catch {
     }
 #Start OSDCloud ZTI the RIGHT way
 Write-Host  -ForegroundColor Cyan "Starting OSDCloud GUI"
+Write-Host  -ForegroundColor Cyan "If you see an Autopilot json file selected, unselect it."
 Write-Host  -ForegroundColor Cyan "Choose your preferred version of Windows Enterprise"
 Write-Host  -ForegroundColor Cyan "Very Important, to decrease the potential for a Blue Screen of Death crash"
 Write-Host  -ForegroundColor Cyan "Choose Microsoft Update Catalog for the DriverPack"
 
-Start-Sleep -Seconds 60
+Start-Sleep -Seconds 15
 Start-OSDCloudGUI
 
 write-host   -ForegroundColor cyan "Before pulling out the USB drive, shutdown the computer with this command"
 write-host   -ForegroundColor cyan "wpeutil shutdown in a command prompt"
-Start-Sleep  -Seconds 60
+Start-Sleep  -Seconds 5
