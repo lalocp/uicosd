dbafd54f53c4b01b39602e92d759990cc2a642bb
lalocp <lalo.camacho@gmail.com>
2024-02-14 20:09:38 -0600

    Update Start-ADJoin.ps1
    added net user add commands

--- Start-ADJoin.ps1
+++ Start-ADJoin.ps1
@@ -6,22 +6,25 @@
 #     02/14/2024 HVD. Initial script v. .01
 ###################################################################
 
+# Add a local admin account
+net user lansoft /add
+net localgroup administrators lansoft /add
+
 # Get Credentials of user with bind permissions
-
 Write-Host "Please enter the username and password when prompted"
-
 Sleep 5
 
 $creds = get-credential
 
 # Execute Join operation
-
 $computerName = (Get-ComputerInfo).csCaption
-
 $domainName = "ad.uic.edu"
+Write-Host "Computer $computerName will be joined to $domainName"
+Sleep 2
+Add-Computer -DomainName $domainName -Credential $creds -Force
 
-Write-Host "Computer $computerName will be joined to $domainName and restart"
-
-Sleep 10
-
-Add-Computer -DomainName $domainName -Credential $creds -Restart -Force
+# Remove defaultuser0
+net user defaultuser0 /delete
+Write-Host "Restarting computer in 10 seconds"
+sleep 10
+Restart-Computer -force
