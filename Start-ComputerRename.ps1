0a38d892d39a31357498fcd6aafe1fff3218a572
lalocp <lalo.camacho@gmail.com>
2024-02-14 20:15:46 -0600

    Update Start-ComputerRename.ps1

--- Start-ComputerRename.ps1
+++ Start-ComputerRename.ps1
@@ -9,31 +9,25 @@
 ###################################################################
 
 # Disable Privacy
-
 WRITE-HOST "Disabling Privacy Consent..."
-
-New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\OOBE" -Name "PrivacyConsentStatus" -PropertyType DWORD -Value 1 -Force
+New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\OOBE" -Name "PrivacyConsentStatus" -PropertyType DWORD -Value 1 -Force | out-null
 
 # Disable OOBE
-
 WRITE-HOST "Disabling OOBE..."
-
-New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\OOBE" -Name "SkipMachineOOBE" -PropertyType DWORD -Value 1 -Force
+New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\OOBE" -Name "SkipMachineOOBE" -PropertyType DWORD -Value 1 -Force | out-null
+New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\OOBE" -Name "LaunchUserOOBE" -PropertyType DWORD -Value 0 -Force | out-null
 
 # Disable Telemetry
-
 WRITE-HOST "Disabling Telemetry..."
-
-New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\DataCollection" -Name "AllowTelemetry" -PropertyType DWORD -Value 0 -Force
+New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\DataCollection" -Name "AllowTelemetry" -PropertyType DWORD -Value 0 -Force| out-null
 
 # Rename Computer
-
 $newname = Read-Host "Please enter the new name of the computer"
 
-WRITE-HOST "The computer will be renamed to $newname and restart in 10 seconds."
+sleep 2
+WRITE-HOST "The computer will be renamed to $($newname) and restart in 5 seconds."
 WRITE-HOST "Press control C to cancel"
 
-Sleep 15
+Sleep 8
 
 Rename-Computer -NewName $newname -Force -Restart
-
