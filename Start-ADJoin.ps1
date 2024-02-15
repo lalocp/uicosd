bc26cf788cc312c639eafbde6c5c4a784ed814a6
lalocp <lalo.camacho@gmail.com>
2024-02-14 21:01:23 -0600

    Update Start-ADJoin.ps1

--- Start-ADJoin.ps1
+++ Start-ADJoin.ps1
@@ -7,7 +7,7 @@
 ###################################################################
 
 # Add a local admin account
-net user lansoft /add
+net user lansoft /add 
 net localgroup administrators lansoft /add
 
 # Remove Autologon
@@ -21,18 +21,17 @@ Get-WindowsUpdate -Severity Important -Install -Download -IgnoreReboot
 
 # Get Credentials of user with bind permissions
 Write-Host "Please enter the username and password when prompted"
-Sleep 2
 $creds = get-credential
 
 # Execute Join operation
 $computerName = (Get-ComputerInfo).csCaption
 $domainName = "ad.uic.edu"
-Write-Host "Computer $computerName will be joined to $domainName"
-Sleep 2
+Write-Host "Computer $($computerName) will be joined to $domainName"
+Sleep 1
 Add-Computer -DomainName $domainName -Credential $creds -Force
 
 # Remove defaultuser0
 net user defaultuser0 /delete
-Write-Host "Restarting computer in 10 seconds"
-sleep 10
+Write-Host "Restarting computer in 5 seconds"
+sleep 5
 Restart-Computer -force
