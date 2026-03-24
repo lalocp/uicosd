9161d599d91992f3ce4f3cedc0e1dd9b7c504752
lalocp <lalo.camacho@gmail.com>
2026-03-24 18:42:12 -0500

    Update getmouse.ps1

--- getmouse.ps1
+++ getmouse.ps1
@@ -2,3 +2,5 @@ $mouse = Get-WmiObject Win32_PnPEntity | Where-Object { $_.Service -eq 'mouhid' 
 $mouse.Disable()
 Start-Sleep -s 2
 $mouse.Enable()
+$m = Get-WmiObject Win32_PnPEntity | Where-Object { $_.Service -eq "i8042prt" -or $_.Service -eq "mouclass" }
+if ($m) { $m | ForEach-Object { $_.Disable(); Start-Sleep 1; $_.Enable() } }
