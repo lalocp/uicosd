c15f9a57877369db8cac6710b659b68b5cfcc50b
lalocp <lalo.camacho@gmail.com>
2026-03-24 18:22:50 -0500

    Update getmouse.ps1

--- getmouse.ps1
+++ getmouse.ps1
@@ -1,4 +1,4 @@
-Get-PnpDevice -ClassName Mouse | ForEach-Object {
-    Disable-PnpDevice -InstanceId $_.InstanceId -Confirm:$false
-    Enable-PnpDevice -InstanceId $_.InstanceId -Confirm:$false
-}
+$mouse = Get-WmiObject Win32_PnPEntity | Where-Object { $_.Service -eq 'mouhid' -or $_.Name -like "*mouse*" }
+$mouse.Disable()
+Start-Sleep -s 2
+$mouse.Enable()
